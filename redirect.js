6a5dff8e7b33e40ffc64977b626b2596f9a2b74d
Brad Zhao <buwang0711@gmail.com>
2025-04-21 15:03:47 +0800

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,9 +1,3 @@
-// redirect.js
-const localServer = 'http://<YOUR_LOCAL_SERVER_IP>/mediawiki/';
-const path = window.location.pathname; // Get the current path
-
-// Append the path to the local server URL
-const redirectUrl = localServer + path.substring(1); // Remove the leading '/' from the path
-
-// Redirect to the local server
-window.location.href = redirectUrl;
+var currentPath = window.location.pathname;
+var newUrl = 'http://27.125.172.55/' + currentPath;
+window.location.replace(newUrl);
