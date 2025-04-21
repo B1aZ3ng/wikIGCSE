9c72d6acce6f3ad306c386ac42e09811e6a7e1b3
Brad Zhao <buwang0711@gmail.com>
2025-04-21 14:56:30 +0800

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,5 +1,9 @@
-(function() {
-  var currentPath = window.location.pathname;
-  var newUrl = 'https://b1az3ng.github.io/wikIGCSE' + currentPath;
-  window.location.replace(newUrl);
-})();
+// redirect.js
+const localServer = 'http://<YOUR_LOCAL_SERVER_IP>/mediawiki/';
+const path = window.location.pathname; // Get the current path
+
+// Append the path to the local server URL
+const redirectUrl = localServer + path.substring(1); // Remove the leading '/' from the path
+
+// Redirect to the local server
+window.location.href = redirectUrl;
